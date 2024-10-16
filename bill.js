f641e28480710993b6d77f0b23a7cb63cd432a50
ponczuTM <mroczkowskioliwer10@gmail.com>
2024-10-16 15:39:27 +0200

    test

--- bill.js
+++ bill.js
@@ -1,25 +1,19 @@
-const printer = require("printer");
+const { exec } = require("child_process");
 
-function printMessage(message) {
-  const printers = printer.getPrinters();
+function printText(text) {
+  const command = `echo ${text} > LPT1`;
 
-  const selectedPrinter = printers.find((printer) => printer.name === "LPT1");
-
-  if (selectedPrinter) {
-    printer.printDirect({
-      data: message,
-      printer: selectedPrinter.name,
-      type: "RAW",
-      success: function (jobID) {
-        console.log(`Wysłano wydruk z ID: ${jobID}`);
-      },
-      error: function (err) {
-        console.error(`Błąd podczas drukowania: ${err}`);
-      },
-    });
-  } else {
-    console.error("Drukarka nie została znaleziona.");
-  }
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Błąd podczas drukowania: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Błąd: ${stderr}`);
+      return;
+    }
+    console.log(`Wydrukowano: ${stdout}`);
+  });
 }
 
-printMessage("CZEŚĆ");
+printText("CZEŚĆ");
